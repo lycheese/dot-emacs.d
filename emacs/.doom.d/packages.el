1e41697e863b696ebebe528b5e7404a24ef95ab7
lycheese <4779944-lycheese@users.noreply.gitlab.com>
2021-01-11 14:59:38 +0100

    Free dotfiles from the nixos config
    Restore dotfiles to old directory structure and diff with master to
    get back to its original purpose.

---
+++ emacs/.doom.d/packages.el
@@ -0,0 +1,19 @@
+;; -*- no-byte-compile: t; -*-
+;;; $DOOMDIR/packages.el
+
+;; (package! lsp-julia :recipe (:host github :repo "non-jedi/lsp-julia"))
+(package! anki-editor)
+
+(package! org-super-agenda)
+;; (package! org-caldav :recipe (:host github :repo "dengste/org-caldav"))
+
+(package! hyperbole)
+
+(package! modus-operandi-theme)
+(package! modus-vivendi-theme)
+
+(package! git-auto-commit-mode)
+
+(package! fish-mode)
+
+(package! ddskk)
