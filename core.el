c680481e843feed472aa413c28e6bc83cc195bdd
lycheese <dev@lycheese.org>
2021-11-18 12:21:37 +0100

    feat(core): initial commit

---
+++ core.el
@@ -0,0 +1,4 @@
+;; Handling backups
+(setq backup-directory-alist `(("~/.emacs.d/etc/backups")))
+
+(load (concat user-emacs-directory "programming"))
---
+++ init.el
@@ -0,0 +1,32 @@
+(set-language-environment "UTF-8")
+(setq default-input-method nil)
+
+(load-theme 'tsdh-dark)
+
+(push '(menu-bar-lines . 0)   default-frame-alist)
+(push '(tool-bar-lines . 0)   default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
+(setq menu-bar-mode nil
+      tool-bar-mode nil
+      scroll-bar-mode nil)
+
+(setq straight-use-package-by-default t)
+
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(straight-use-package 'use-package)
+
+(setq user-emacs-directory (file-name-directory load-file-name))
+
+(load (concat user-emacs-directory "core"))
