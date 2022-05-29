d9deeb102955184cf4e3ea621c1ce6efc4796c53
lycheese <dev@lycheese.org>
2022-05-29 12:07:14 +0200

    feat: add sudo command

--- core/nyx-core.el
+++ core/nyx-core.el
@@ -15,5 +15,22 @@
     "(provide '" (file-name-base (buffer-file-name)) ")\n"
     ";;; " (file-name-nondirectory (buffer-file-name)) "ends here"))
 
+(defun nyx-sudo ()
+  "Use TRAMP to `sudo' the current buffer"
+  (interactive)
+  (when buffer-file-name
+    (if (file-remote-p buffer-file-name)
+	(let* ((remote-path (file-remote-p buffer-file-name))
+	       (host (substring remote-path (+ (string-match "@\\(.*\\):$" remote-path) 1) -1))
+	       (file-path (substring buffer-file-name (string-match "[^:]*$" buffer-file-name))))
+	  (find-alternate-file
+	   (concat (substring remote-path 0 -1)
+		   "|sudo:" host ":" file-path)))
+      (find-alternate-file
+       (concat "/sudo:root@localhost:"
+	       buffer-file-name)))))
+
+
+
 (provide 'nyx-core)
 ;;; nyx-core.elends here
