27164a8674a8513aaf63e1eda3fca2760e09fda1
lycheese <dev@lycheese.org>
2022-06-01 12:36:28 +0200

    feat(clojure): use lispy instead of puni

--- modules/nyx-clojure.el
+++ modules/nyx-clojure.el
@@ -4,5 +4,8 @@
 (dolist (package '(clojure-mode cider))
   (straight-use-package package))
 
+(add-hook 'clojure-mode-hook #'puni-disable-puni-mode)
+(add-hook 'clojure-mode-hook #'enable-lispy)
+
 (provide 'nyx-clojure)
 ;;; nyx-clojure.elends here
