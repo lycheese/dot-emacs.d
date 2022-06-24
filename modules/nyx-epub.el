c5d9a6b8aa530868a93ff4cd35ce329238d0415b
lycheese <dev@lycheese.org>
2022-06-24 14:35:31 +0200

    feat(modules): add epub module

---
+++ modules/nyx-epub.el
@@ -0,0 +1,30 @@
+;;; nyx-epub.el --- Module for epub files            -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022
+
+;; Author:  <lycheese@nanoforge>
+;; Keywords:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+(straight-use-package 'nov)
+
+
+(provide 'nyx-epub)
+;;; nyx-epub.el ends here
