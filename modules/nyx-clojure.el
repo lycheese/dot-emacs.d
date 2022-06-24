fc6a2d6026ef1c2708935134e1e0fcc5d7fb3044
lycheese <dev@lycheese.org>
2022-06-24 14:32:29 +0200

    feat(clojure): add eglot support
    - Enable treating forms in comments as toplevel
    - Do not auto-select doc buffers

--- modules/nyx-clojure.el
+++ modules/nyx-clojure.el
@@ -1,11 +1,14 @@
 ;;; nyx-clojure.el -*- lexical-binding: t; -*-
 
 ;;; Code:
-(dolist (package '(clojure-mode cider))
-  (straight-use-package package))
+(straight-use-package 'cider)
+
+(setq cider-doc-auto-select-buffer nil
+      clojure-toplevel-inside-comment-form t)
 
 (add-hook 'clojure-mode-hook #'puni-disable-puni-mode)
 (add-hook 'clojure-mode-hook #'enable-lispy)
+(add-hook 'clojure-mode-hook #'eglot-ensure)
 
 (provide 'nyx-clojure)
 ;;; nyx-clojure.elends here
